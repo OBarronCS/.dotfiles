2823a75e18b8153862534de2f6bcac8aa3e6d101
O B <otsobnalle@gmail.com>
2023-09-14 21:18:57 -0700

    readme symlink test

---
+++ setup.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+cd ~
+git clone https://github.com/OBarronCS/.dotfiles.git
+
+# These lines will fail if they would override existing files
+ln -s ~/.dotfiles/.vimrc ~/.vimrc 
+ln -s ~/.dotfiles/.tmux.conf ~/.tmux.conf 
+ln -s ~/.dotfiles/.inputrc ~/.inputrc
+
+cd -
+
+# Install fzf
+git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
+~/.fzf/install --all
+
+# Install tmux bash autocomplete
+curl https://raw.githubusercontent.com/imomaliev/tmux-bash-completion/master/completions/tmux | sudo tee /etc/bash_completion.d/tmux > /dev/null
+
+# Install vim-plug
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+vim +'PlugInstall --sync' +qa
+
+# Install tmux plugin manager
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+
+
+# Install tmux plugins
+~/.tmux/plugins/tpm/bin/install_plugins
+
