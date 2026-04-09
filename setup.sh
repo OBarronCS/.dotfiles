c8a6e641bed32b8c3a808994d389ef20c3d7aafb
OB <obarroncs@gmail.com>
2026-04-09 14:02:41 -0400

    fix

--- setup.sh
+++ setup.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 
+RED="\e[31m"
+GREEN="\e[32m"
+YELLOW="\e[33m"
+ENDCOLOR="\e[0m"
+
+function print_info {
+    echo "--------------------"
+    echo -e "${GREEN}$1${ENDCOLOR}"
+    echo "--------------------"
+}
+
 # These lines will fail if they would override existing files
 ln -s ~/.dotfiles/.vimrc ~/.vimrc 
 ln -s ~/.dotfiles/.tmux.conf ~/.tmux.conf 
@@ -7,20 +18,22 @@ ln -s ~/.dotfiles/.inputrc ~/.inputrc
 mkdir -p ~/.config/kitty
 ln -s ~/.dotfiles/kitty.conf ~/.config/kitty/kitty.conf
 
-# Install fzf
+
+print_info "Installing fzf"
 if [ ! -d "${HOME}/.fzf" ]; then
     git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
     ~/.fzf/install --all
 fi
 
-
 # If we have sudo, and can run it without prompting
 if command -v sudo &> /dev/null && sudo -n true 2> /dev/null; then
     # Install tmux bash autocomplete
+    print_info "Installing tmux bash autocompletions"
     curl https://raw.githubusercontent.com/imomaliev/tmux-bash-completion/master/completions/tmux | sudo tee /etc/bash_completion.d/tmux > /dev/null
 fi
  
 # Install vim-plug
+print_info "Installing vim-plug"
 if [ ! -f "${HOME}/.vim/autoload/plug.vim" ]; then
 
     curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
@@ -29,12 +42,10 @@ if [ ! -f "${HOME}/.vim/autoload/plug.vim" ]; then
     vim +'PlugInstall --sync' +qa
 fi
 
-# Install tmux plugin manager
+print_info "Installing tmux plugin manager"
 if [ ! -d "${HOME}/.tmux/plugins/tpm" ]; then
     mkdir -p ~/.tmux/plugins
     git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
     ~/.tmux/plugins/tpm/bin/install_plugins
 fi
-
-
 
