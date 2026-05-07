f88a18086b17e708641569f5d9b1ea71ee2b32b9
OB <obarroncs@gmail.com>
2026-05-06 23:38:03 -0400

    Check that git, wget, and curl are installed

--- setup.sh
+++ setup.sh
@@ -18,6 +18,38 @@ function print_info {
     echo "--------------------"
 }
 
+function print_error {
+    echo "--------------------"
+    echo -e "${RED}$1${ENDCOLOR}"
+    echo "--------------------"
+}
+
+fetch_to_file() {
+    local url="$1"
+    local dest="$2"
+    mkdir -p "$(dirname "$dest")"
+    if command -v curl >/dev/null 2>&1; then
+        curl -LsSfo "$dest" "$url"
+    elif command -v wget >/dev/null 2>&1; then
+        wget -O "$dest" "$url"
+    else
+        print_error "Error: neither curl nor wget is installed" >&2
+        return 1
+    fi
+}
+
+fetch_to_stdout() {
+    local url="$1"
+    if command -v curl >/dev/null 2>&1; then
+        curl -LsSf "$url"
+    elif command -v wget >/dev/null 2>&1; then
+        wget -qO- "$url"
+    else
+        print_error "Error: neither curl nor wget is installed" >&2
+        return 1
+    fi
+}
+
 # These lines will fail if they would override existing files
 ln -s ~/.dotfiles/.vimrc ~/.vimrc 2>/dev/null || true
 ln -s ~/.dotfiles/.tmux.conf ~/.tmux.conf 2>/dev/null || true
@@ -28,8 +60,12 @@ ln -s ~/.dotfiles/kitty.conf ~/.config/kitty/kitty.conf 2>/dev/null || true
 
 print_info "Installing fzf"
 if [ ! -d "${HOME}/.fzf" ]; then
-    git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
-    ~/.fzf/install --all
+    if command -v git >/dev/null 2>&1; then
+        git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
+        ~/.fzf/install --all
+    else
+        print_error "Failed to install fzf - git is not installed"
+    fi
 else
     print_info "fzf already installed"
 fi
@@ -37,8 +73,7 @@ fi
 if command -v vim &> /dev/null; then
     print_info "Installing vim-plug"
     if [ ! -f "${HOME}/.vim/autoload/plug.vim" ]; then
-        curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
-            https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+        fetch_to_file https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim ~/.vim/autoload/plug.vim
 
         # Need the shell redirections, otherwise there is chaos.
         # Causing outer script to fail, bash to not track where current command being run is (skips to random point)
@@ -49,15 +84,21 @@ fi
 if command -v tmux &> /dev/null; then
     print_info "Installing tmux plugin manager"
     if [ ! -d "${HOME}/.tmux/plugins/tpm" ]; then
-        mkdir -p ~/.tmux/plugins
-        git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+        if command -v git >/dev/null 2>&1; then
 
-        # If currently in a tmux session, reload the config so the install succeeds
-        if [ -n "$TMUX" ]; then
-            tmux source-file ~/.tmux.conf
+            mkdir -p ~/.tmux/plugins
+            git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+
+            # If currently in a tmux session, reload the config so the install succeeds
+            if [ -n "$TMUX" ]; then
+                tmux source-file ~/.tmux.conf
+            fi
+
+            ~/.tmux/plugins/tpm/bin/install_plugins
+
+        else
+            print_error "Failed to install tmpm - git is not installed"
         fi
-
-        ~/.tmux/plugins/tpm/bin/install_plugins
     fi
 fi
 
@@ -65,7 +106,7 @@ fi
 if command -v sudo &> /dev/null && sudo -n true 2> /dev/null; then
     # Install tmux bash autocomplete
     print_info "Installing tmux bash autocompletions"
-    curl https://raw.githubusercontent.com/imomaliev/tmux-bash-completion/master/completions/tmux | sudo tee /etc/bash_completion.d/tmux > /dev/null
+    fetch_to_stdout https://raw.githubusercontent.com/imomaliev/tmux-bash-completion/master/completions/tmux | sudo tee /etc/bash_completion.d/tmux > /dev/null
 fi
 
 print_info "Done setting up .dotfiles"
